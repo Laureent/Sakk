5df49a0624bba847bb3bb81a3a06f83ac1a930bc
Laurent <segitseg@kerlek.com>
2021-10-22 08:12:49 +0200

    Sakk alapok

---
+++ Sakk/Babuk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakk
+{
+    class Babuk
+    {
+        struct babukPozicio 
+        {
+            int pozicioX;
+            int pozicioY;
+        }
+
+        Dictionary<babukPozicio> paraszt = new Dictionary<babukPozicio>();
+        public void paraszt()
+        {
+            
+        }
+        public Babuk()
+        {
+
+        }
+    }
+}
---
+++ Sakk/Generalas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakk
+{
+    public class tablaGeneralas
+    {
+        public tablaGeneralas()
+        {
+            const int tablameret = 8;
+            int[,] tabla = new int[8, 8];
+            for (int i = 0; i < tablameret; i++)
+            {
+                for (int h = 0; h < tablameret; h++)
+                {
+                    tabla[i,h] = 0;
+                }
+            }
+        }
+    }
+}
