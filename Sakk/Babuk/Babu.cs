de0f9fb4f9c832aaea12e47c7d333414688c8c63
padarkristof2 <padarkristof2@gmail.com>
2021-11-23 17:16:25 +0100

    bábu osztályok

---
+++ Sakk/Babuk/Babu.cs
@@ -0,0 +1,23 @@
+﻿namespace Sakk.Babuk
+{
+    public class Babu : Mezo
+    {
+        public Babu(int sor, int oszlop) : base(sor, oszlop)
+        {
+
+        }
+
+        public virtual void LepesBeallitas(Mezo babuHelyzete, Tabla tabla)
+        {
+            if (babuHelyzete.babuTipus is Paraszt)
+            {
+                new Paraszt(sor, oszlop).LepesBeallitas(babuHelyzete, tabla);
+            }
+            else if (babuHelyzete.babuTipus is Bastya)
+            {
+                new Bastya(sor, oszlop).LepesBeallitas(babuHelyzete, tabla);
+            }
+            new Paraszt(sor, oszlop).LepesBeallitas(babuHelyzete, tabla);
+        }
+    }
+}
